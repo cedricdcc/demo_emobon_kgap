cb98409df816cca2f8697d7333c15022e8c63f73
cedricd <cedricdecruw@gmail.com>
2024-06-07 16:07:59 +0200

    chore: Add .gitignore, requirements.txt, and docker-compose.yml files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # demo_graphdb_ollama
 a small demo that uses graphdb and ollama for natural language querying of RDF data
+
+# Installation
+
+run the following commands
+
+```bash	
+python -m venv venv
+source venv/Scripts/activate
+pip install -r requirements.txt
+docker-compose up 
+python demo.py
+```
+
+in demo.py change DEMO_QUESTION to get a different query
---
+++ demo.py
@@ -0,0 +1,24 @@
+from langchain_community.graphs import OntotextGraphDBGraph
+from langchain_community.llms import ollama
+from langchain.chains.graph_qa.ontotext_graphdb import OntotextGraphDBQAChain
+
+READ_URI_STORE = "http://localhost:7200/repositories/kgap"
+WRITE_URI_STORE = "http://localhost:7200/repositories/kgap/statements"
+
+DEMO_QUESTION = "How many observations are there?"
+
+
+# the main problem that i'm facing right now is the query ontology
+# how are we going to define our classes
+graph = OntotextGraphDBGraph(
+    query_endpoint=READ_URI_STORE,
+    query_ontology="CONSTRUCT  {?s ?p ?o} FROM <urn:sync:emobon_ontology.ttl> WHERE {?s ?p ?o}",
+)
+
+# specify ollama endpoint
+
+llama_three = ollama.Ollama(model="llama3:8b")
+
+chain = OntotextGraphDBQAChain.from_llm(llama_three, graph=graph, verbose=True)
+
+chain.invoke({chain.input_key: DEMO_QUESTION})[chain.output_key]
