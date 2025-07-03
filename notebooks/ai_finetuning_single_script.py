fefdf976585bebcedb9ac0c7422dca678e60dbac
cedricdcc <cedricdecruw@gmail.com>
2025-07-03 11:42:02 +0200

    Adjust batch size for training dataloader to optimize performance

--- notebooks/ai_finetuning_single_script.py
+++ notebooks/ai_finetuning_single_script.py
@@ -99,7 +99,7 @@ print(processed_ds)
 train_ds = processed_ds["train"]
 eval_ds = processed_ds["test"]
 
-batch_size = 8  # Adjust batch size as needed
+batch_size = 1  # Adjust batch size as needed
 
 train_dataloader = DataLoader(
     train_ds,
